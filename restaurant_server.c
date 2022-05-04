5104e9d0f8fce12e7842eb4a53ec11a41b80e832
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-05-04 13:20:00 +0900

    server 기본 구조 작성

--- restaurant_server.c
+++ restaurant_server.c
@@ -1,3 +1,22 @@
+/* 
+ *  파일명: restaurant_server.c
+ *  
+ *  # 서버(식당)가 커져 있어야 클라이언트가 접속이 가능
+ *    →  클라이언트 소켓이 종료되더라도 다른 클라이언트의 접속을 받아야 하기 때문에
+ *       계속 대기해야 함
+ *  
+ *  1. 클라이언트 연결 -> 메뉴 전송
+ *
+ *  2. 주문 요청: 클라이언트: 주문 구조체 형태로 전송 →  이를 서버의 주문 내역에 저장.
+ *                  서버: 클라이언트에게 주문이 완료됨을 전송(문자열)
+ *  
+ *  3. 주문 내역은 동적 리스트로 저장
+ *
+ *   +) 추가 기능
+ *     절차적 수행이라 서버 기능과 주문 내역 관리가 동시에 이루어지기 어려움.
+ *     따라서, 서버 기능과 주문 내역 관리 기능을 Thread로 관리할 필요 있음. (고려)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +25,71 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#define BUF 1024
+
 int main(int argc, char** argv){
+	int server_sock;
+	int client_sock;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	int client_addr_size;
+
+	int str_len;
+	int request_num;
+	char[BUF] msg;
+
+	// order list 동적 리스트 필요(order 구조체 작성 완료후)
+
+	if(argc != 2){
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
 	
+	// socket()
+	server_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(server_sock < 0){
+		perror("socket creation error\n");
+		exit(1);
+	}
+	
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(atoi(argv[1]));
+
+	// bind()
+	if(bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+		perror("socket bind error\n");
+		exit(1);
+	}
+
+	// listen() 
+	if(listen(server_sock, 5) < 0){
+		peeror("listen error");
+		exit(1);
+	}
+
+	client_addr_size = sizeof(client_addr);
+
+	// function
+	while(1){
+		// connect() -> send menu to client
+		client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);
+		if(client_sock < 0){
+			perror("socket accept error\n");
+			exit(1);
+		}
+		write(client_sock, menu, sizeof(menu));
+		
+
+		// 주문 대기
+			// 1. read로 클라이언트 소켓에서 order 내용 입력받기 -> neworder
+			// 2. neworder를 리스트에 넣기
+			// 3. 화면에 새로운 주문 내역 출력
+		
+		
+
+		// close()
+		close(client_sock);
+	}		
 }
