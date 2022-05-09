03f39bc10200bb80a1d19b88cbc892183788a470
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-05-09 14:17:10 +0900

    order 내용 추가
    total, list 수량만 전송

--- struct.h
+++ struct.h
@@ -3,7 +3,8 @@ typedef struct order{
 	char name[20];	// 주문자 이름(클라이언트)
 	char phone[20];	// 주문자 휴대번호(클라이언트)
 	char add[20];	// 주문 배송지(클라이언트)
-	int** list;	// 주문 리스트 [menu_len][2]
+	int* list;	// 주문 리스트 [menu_len][2]
+	int total;
 	struct order* next;
 } order;
 
