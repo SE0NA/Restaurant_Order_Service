a4ac64ca59abf028000b3e7aa8296425ecf81a99
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-05-09 14:50:05 +0900

    test: server - read 확인용

--- restaurant_server.c
+++ restaurant_server.c
@@ -139,11 +139,14 @@ int main(int argc, char** argv){
 				orderlist_t = neworder;
 			}
 
-			if(n = read(client_sock, (void*)&neworder, sizeof(order))<0){
+			printf("order 생성");
+			if(n = read(client_sock, (void*)neworder, sizeof(order))<0){
 				printf("read 오류!\n");
 				close(client_sock);
 				exit(0);
 			}
+			
+
 			neworder->no = ++request_num;
 			printf("%d %d %s\n", neworder->no, neworder->list[0], neworder->name);
 
