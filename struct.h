a5948b278628711d7fada0f0d226496b2c279cbc
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-05-09 15:33:18 +0900

    현재 진행 내용 추가
    - 데이터 전송 확인 완료

--- struct.h
+++ struct.h
@@ -2,8 +2,7 @@ typedef struct order{
 	int no;		// 주문번호(서버)
 	char name[20];	// 주문자 이름(클라이언트)
 	char phone[20];	// 주문자 휴대번호(클라이언트)
-	char add[20];	// 주문 배송지(클라이언트)
-	int* list;	// 주문 리스트 [count]: index=menu num
+	char addr[20];	// 주문 배송지(클라이언트)
 	char list_str[256];
 	int total;
 	struct order* next;
@@ -13,7 +12,5 @@ typedef struct menu{
 	int menu_len;		// 총 메뉴 수
 	char cost_str[256];
 	char menu_str[254];
-	
 	int* menu_cost;
-//	char** menu_str;	// 메뉴 내용
 } menu;
