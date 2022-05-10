c437acdd45ed8c8c0a81918a74010b012aab5701
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-05-10 22:36:36 +0900

    server3: total 표시

--- restaurant_server3.c
+++ restaurant_server3.c
@@ -136,7 +136,6 @@ int main(int argc, char** argv){
 
 			// 2) read order from client
 			n = read(client_sock, (void*)&neworder, sizeof(order));
-			neworder.total = 0;
 			timer = time(NULL);
 			t = localtime(&timer);
 			sprintf(msg, "%02d-%02d-%02d %02d:%02d:%02d", t->tm_year+1900, t->tm_mon+1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
