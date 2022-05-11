d4f44b25cfb60f00939af47d1a19e10742de561f
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-05-11 14:36:45 +0900

    client: 메뉴 선택 UI(방향키)
    전송 X

--- customer_client.c
+++ customer_client.c
@@ -16,10 +16,15 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <termios.h>
 #include "struct.h"
 
 #define BUF 512
 #define IPserver "127.0.0.1"
+
+void inputOrderlist(order*, menu*);
+void inputCustomerInformation(order*);
+int getkey();
 
 int main(int argc, char**argv){
 	int sock;
@@ -58,44 +63,32 @@ int main(int argc, char**argv){
 	}
 	//메뉴 읽기
 	str_len = read(sock, (void*)&mymenu, sizeof(menu));
-
-
-	//메뉴 출력
-	printf("%s\n%s\n", mymenu.cost_str, mymenu.menu_str);
-
-	//메뉴 선택(쓰기)
-	printf("이름: ");
-	fgets(msg, sizeof(msg), stdin);
-	msg[strlen(msg)-1] = 0;
-	strcpy(myorder.name, msg);
-
-	printf("전화번호: ");
-	fgets(msg, sizeof(msg), stdin);
-	msg[strlen(msg)-1] = 0;
-	strcpy(myorder.phone, msg);
-
-	printf("주소: ");
-	fgets(msg, sizeof(msg), stdin);
-	msg[strlen(msg)-1] = 0;
-	strcpy(myorder.addr, msg);
-
-	printf("주문(n@n@n@n): ");
-	fgets(msg, sizeof(msg), stdin);
-	msg[strlen(msg)-1] = 0;
-	strcpy(myorder.list_str, msg);
+	printf("%s\n", mymenu.menu_str);
 	
-	printf("총액: ");
-	fgets(msg, sizeof(msg), stdin);
-	msg[strlen(msg)-1] = 0;
-	myorder.total = atoi(msg);
-	/*
-	strcpy(myorder.phone, "010-0000-0000");
-	strcpy(myorder.addr, "수원시");
-	strcpy(myorder.list_str, "1@3@0@0");
-	myorder.total = 10000;*/
+	//메뉴 선택
+	inputOrderlist(&myorder, &mymenu);
+	printf("%s\n\n", myorder.list_str);
+
+	
+	// 임시
+	printf("이름: ");
+        fgets(msg, sizeof(msg), stdin);
+        msg[strlen(msg)-1] = 0;
+        strcpy(myorder.name, msg);
+
+        printf("전화번호: ");
+        fgets(msg, sizeof(msg), stdin);
+        msg[strlen(msg)-1] = 0;
+        strcpy(myorder.phone, msg);
+
+        printf("주소: ");
+        fgets(msg, sizeof(msg), stdin);
+        msg[strlen(msg)-1] = 0;
+        strcpy(myorder.addr, msg);
 
 
-	//메뉴 선택 완료
+
+
 	//서버로 보내기
 	write(sock, (void*)&myorder, sizeof(order));
 
@@ -109,9 +102,7 @@ int main(int argc, char**argv){
 	// connect -> menu를 전송 받음
 	//str_len = read(sock, msg, BUF-1);
 	
-
-	// order 매개변수 입력 필요
-		
+	
 
 	// order 전송
 	// write(sock, myorder, sizeof(order));
@@ -121,4 +112,122 @@ int main(int argc, char**argv){
 	close(sock);
 
 	return 0;
+}
+
+// 메뉴 수량 입력(방향키)
+void inputOrderlist(order* myorder, menu* mymenu){
+	char **menu_list, *str, *str2;
+	int *cost_list, *order_list;
+	int total = 0, row = 0;
+	char key=0;
+	
+	// 메뉴 내용 저장을 위한 메모리 동적할당
+	menu_list = (char**)malloc(sizeof(char*)*mymenu->menu_len);	// 메뉴 이름 저장
+	cost_list = (int*)malloc(sizeof(int)*mymenu->menu_len);		// 가격 저장
+	order_list = (int*)malloc(sizeof(int)*mymenu->menu_len);	// (입력) 주문 수량
+	for(int i=0;i<mymenu->menu_len;i++){
+		menu_list[i] = (char*)malloc(sizeof(char)*50);
+		cost_list[i] = 0;
+		order_list[i] = 0;
+	}
+	// 내용 저장
+	str = strtok(mymenu->menu_str, "@");	// 메뉴 이름
+	strcpy(menu_list[0], str);
+	for(int i=1;i<mymenu->menu_len;i++){
+		str = strtok(NULL, "@");
+		strcpy(menu_list[i], str);
+	}
+	str = strtok(mymenu->cost_str, "@");	// 메뉴 가격
+	cost_list[0] = atoi(str);
+	for(int i=1;i<mymenu->menu_len;i++){
+		str = strtok(NULL, "@");
+		cost_list[i] = atoi(str);
+	}
+	
+	while(1){
+		system("clear");	// 화면 초기화
+		printf("+-------------------------------------------------------------------+\n");
+		for(int i=0;i<mymenu->menu_len;i++){	// 메뉴 출력
+			printf("| ");
+			if(row == i){	// 현재 선택된 메뉴
+				printf("%c[1;33m", 27);	// 글자색 변경-> 노랑
+				printf("%s\t\t%d won\t\t", menu_list[i], cost_list[i]);
+				printf("%c[1;36m", 27);	// 글자색 변경 -> 밝은 파랑
+				printf("◀  %2d ▶   ", order_list[i]);
+				printf("%c[0m", 27);	// 글자색 원래대로
+			}
+			else{
+				printf("%s\t\t%d won\t", menu_list[i], cost_list[i]);
+				if(order_list[i] == 0)
+					printf("          ");
+				else
+					printf("   %2d     ", order_list[i]);
+			}
+			printf("\n");
+		}
+		printf("\n\t\t\ttotal: ");
+		printf("%c[1;36m", 27);		// 글자색 변경 -> 밝은 파랑
+		printf(" %d ", total);		// 총 액 출력
+		printf("%c[0m", 27);		// 글자색 원래대로
+		printf("won \n");
+		printf("+-------------------------------------------------------------------+\n");
+
+		// 메뉴 선택(방향키)
+		key = getkey();
+		switch(key){
+			case 65:	// ↑ 키
+				if(row > 0)	row--;
+				break;
+			case 66: 	// ↓ 키
+				if(row < mymenu->menu_len-1)	row++;
+				break;
+			case 68:	// ← 키
+				if(order_list[row] > 0){
+					order_list[row]--;
+					total -= cost_list[row];
+				}
+				break;
+			case 67:	// → 키
+				if(order_list[row] < 99){
+					order_list[row]++;
+					total += cost_list[row];
+				}
+				break;
+		//	case 10:
+		//		break;
+			default:
+				break;
+		}
+	}
+/*
+	// 주문 정보 정리
+	sprintf(str, "%d", order_list[0]);
+	for(int i=1;i<mymenu->menu_len;i++){
+		sprintf(str2, "@%d", order_list[i]);
+		strcat(str, str2);
+	}
+	strcpy(myorder->list_str, str);
+	myorder->total = total;
+*/
+}
+
+// 주문자 정보 입력
+void inputCustomerInformation(order* myorder){
+
+}
+
+int getkey(){
+	int ch;
+	struct termios old, current;
+
+	tcgetattr(0, &old);
+	current = old;
+
+	current.c_lflag &= ~(ICANON|ECHO);	// 입력값 화면 표시X
+	current.c_cc[VMIN] = 1;
+	current.c_cc[VTIME] = 0;
+	tcsetattr(0, TCSANOW, &current);
+	ch = getchar();
+	tcsetattr(0, TCSANOW, &old);
+	return ch;
 }
